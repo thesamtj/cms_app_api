60ab830475fbf934627be8677a20a20b2d033f49
Samuel Tijani <omalitijani@gmail.com>
2020-10-07 10:25:31 +0100

    Define custom theme

---
+++ LoggingService/CustomConsoleTheme.cs
@@ -0,0 +1,50 @@
+﻿using Serilog.Sinks.SystemConsole.Themes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LoggingService
+{
+    public class CustomConsoleTheme : ConsoleTheme
+    {
+        private readonly IReadOnlyDictionary<ConsoleThemeStyle, string> _styles;
+
+        public CustomConsoleTheme(IReadOnlyDictionary<ConsoleThemeStyle, string> styles)
+        {
+            if (styles == null)
+            {
+                throw new ArgumentNullException(nameof(styles));
+            }
+
+            this._styles = styles.ToDictionary(kv => kv.Key, kv => kv.Value);
+        }
+
+        public static CustomConsoleTheme VisualStudioLight { get; } = CustomConsoleThemes.VisualStudioLight;
+
+        public override bool CanBuffer
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        protected override int ResetCharCount { get; } = "\x001B[0m".Length;
+
+        public override int Set(TextWriter output, ConsoleThemeStyle style)
+        {
+            string str;
+            if (!this._styles.TryGetValue(style, out str))
+                return 0;
+            output.Write(str);
+            return str.Length;
+        }
+
+        public override void Reset(TextWriter output)
+        {
+            output.Write("\x001B[0m");
+        }
+    }
+}
--- cms-app-apicore/Program.cs
+++ cms-app-apicore/Program.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace cms_app_apicore
 {
@@ -13,13 +15,41 @@ namespace cms_app_apicore
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            
+            using (var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    int zero = 0;
+                    int result = 100 / zero;
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Log.Error("An error occured while sending the database {Error} {StackTrace} {InnerException} {Source}", ex.Message, ex.StackTrace, ex.InnerException, ex.Source);
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+                     .Enrich.FromLogContext()
+                     .Enrich.WithProperty("Application", "CMS_APP_API")
+                     .Enrich.WithProperty("MachineName", Environment.MachineName)
+                     .Enrich.WithProperty("CurrentManagedThreadId", Environment.CurrentManagedThreadId)
+                     .Enrich.WithProperty("OSVersion", Environment.OSVersion)
+                     .Enrich.WithProperty("Version", Environment.Version)
+                     .Enrich.WithProperty("UserName", Environment.UserName)
+                     .Enrich.WithProperty("ProcessId", Process.GetCurrentProcess().Id)
+                     .Enrich.WithProperty("ProcessName", Process.GetCurrentProcess().ProcessName)
+                     .WriteTo.Console(theme: CustomConsoleTheme.VisualStudioMacLight)
+                     .WriteTo.File(formatter: new CustomTextFormatter(), path: Path.Combine(hostingContext.HostingEnvironment.ContentRootPath + $"{Path.DirectorySeparatorChar}Logs{Path.DirectorySeparatorChar}", $"cms_app_api_{DateTime.Now:yyyyMMdd}.txt"))
+                    .ReadFrom.Configuration(hostingContext.Configuration));
                     webBuilder.UseStartup<Startup>();
                 });
     }
