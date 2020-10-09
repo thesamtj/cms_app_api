2a4f7369a121b9b0d1b178c658e3e88939f4469c
Samuel Tijani <omalitijani@gmail.com>
2020-10-09 14:33:13 +0100

    Implement user options class

---
+++ ModelService/AppUserOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelService
+{
+    class AppUserOptions
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+        public string Country { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
