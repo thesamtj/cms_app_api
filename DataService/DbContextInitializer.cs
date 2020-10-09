8b70828ab71ec8317f08ef23b274a480fa77c8ef
Samuel Tijani <omalitijani@gmail.com>
2020-10-09 15:16:41 +0100

    Implement DB context initializer

--- DataService/DbContextInitializer.cs
+++ DataService/DbContextInitializer.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using FunctionalService;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DataService
 {
-    class DbContextInitializer
+    public static class DbContextInitializer
     {
+        public static async Task Initialize(DataProtectionKeysContext dataProtectionKeysContext, ApplicationDbContext applicationDbContext, IFunctionalSvc functionalSvc)
+        {
+            // Check, if db DataProtectionKeysContext is created
+            // Check, if db ApplicationDbContext is created
+            await dataProtectionKeysContext.Database.EnsureCreatedAsync();
+            await applicationDbContext.Database.EnsureCreatedAsync();
+
+            // Check, if db contains any users. If db is not empty, then db has been already seeded
+            if (applicationDbContext.ApplicationUsers.Any())
+            {
+                return;
+            }
+
+            // If empty create Admin User and App User
+            await functionalSvc.CreateDefaultAdminUser();
+            await functionalSvc.CreateDefaultUser();
+
+            // Populate Country database 
+            //var countries = await countrySvc.GetCountriesAsync();
+            //if (countries.Count > 0)
+            //{
+            //    await applicationDbContext.Countries.AddRangeAsync(countries);
+            //    await applicationDbContext.SaveChangesAsync();
+            //}
+
+        }
     }
 }
+
