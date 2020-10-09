b96e2e39274d8e9820f39699978b8fb39dd3013e
Samuel Tijani <omalitijani@gmail.com>
2020-10-09 12:40:59 +0100

    Implement data protection

--- DataService/DataProtectionKeysContext.cs
+++ DataService/DataProtectionKeysContext.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DataService
 {
-    class DataProtectionKeysContext
+    public class DataProtectionKeysContext : DbContext, IDataProtectionKeyContext
     {
+        public DataProtectionKeysContext(DbContextOptions<DataProtectionKeysContext> options)
+                    : base(options) { }
+
+        public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
     }
 }
